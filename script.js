1093d0ed85d90b0ce6a08d44c0caa19509ce92a6
FiqryF <fiqryyw@gmail.com>
2025-03-13 15:07:04 +0700

    1

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-button");
+    const navMenu = document.getElementById("nav-menu");
+
+    menuButton.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+        menuButton.classList.toggle("open");
+    });
+});
